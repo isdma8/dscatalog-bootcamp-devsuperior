b126947a4b0caed1db6389f7b20368c89014cd05
Daniel Almeida <daniel.mag.almeida@gmail.com>
2020-10-06 06:33:40 +0100

    database seeding

--- backend/src/main/java/com/isdma/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/isdma/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.isdma.dscatalog.entities.Category;
 import com.isdma.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)//garanto que metodo executa transacção com banco de dados e abre e fecha transação quando deve e alem disso nao travo o bando de dados so para ler dados com o readonly=true
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
