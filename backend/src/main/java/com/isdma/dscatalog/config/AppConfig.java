d44bfea496e0e77103cc2a6abe333e39b4fb4ada
Daniel Almeida <daniel.mag.almeida@gmail.com>
2020-10-28 23:57:41 +0000

    External Configuration

--- backend/src/main/java/com/isdma/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/isdma/dscatalog/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.isdma.dscatalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig { //Uma classe que usamos apra alguma configuração, algum componente qu precisemos
 
+	@Value("${jwt.secret}") //vai buscar valor la do arquivo de configuração, assim em vez de definirmos aqui MY-JWT-SECRET por exemplo definimos la
+	private String jwtSecret; //instancia esta variavel com o valor que estiver no propriety chamado acima
+	
 	@Bean //é um anotation nao de classe mas de metodo
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(); //retorna me uma insancia nova do password enconder, e esta intancia sera um componete gerenciado pelo Spring boot isto porque colocamos o anotation Bean, assim posso injeta lo em outras classes, em outros componentes
@@ -19,7 +23,7 @@ public class AppConfig { //Uma classe que usamos apra alguma configuração, alg
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		tokenConverter.setSigningKey(jwtSecret);
 		return tokenConverter;
 	}
 
--- backend/src/main/java/com/isdma/dscatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/isdma/dscatalog/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.isdma.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,7 +16,17 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-
+	
+	@Value("${security.oauth2.client.client-id}")
+	private String clientId;
+	
+	@Value("${security.oauth2.client.client-secret}")
+	private String clientSecret;
+	
+	@Value("${jwt.duration}")
+	private Integer jwtDuration;
+	
+	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 	
@@ -38,11 +49,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
-		.withClient("dscatalog") //frontend tem de informar destes dados da aplicação no token para ter acesso
-		.secret(passwordEncoder.encode("dscatalog123"))    //mais tarde iremos colocar em arquivo	
+		.withClient(clientId) //frontend tem de informar destes dados da aplicação no token para ter acesso
+		.secret(passwordEncoder.encode(clientSecret))    //mais tarde iremos colocar em arquivo	
 		.scopes("read", "write") //tipo de acesso
 		.authorizedGrantTypes("password") //ha varios tipos no auth, temos de usar o que pretendemos, estao descritos no auth os existentes
-		.accessTokenValiditySeconds(86400); //1 dia em segundos em que o tokin é valido
+		.accessTokenValiditySeconds(jwtDuration); //1 dia em segundos em que o tokin é valido 86400
 	}
 
 	@Override
@@ -50,7 +61,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		//Aqui colocamos quem vai autorizar e qual o formato do tokin
 		endpoints.authenticationManager(authenticationManager) //vai ser o bean que criei
 		.tokenStore(tokenStore) //vai ser o bean que injetamos tambem
-		.accessTokenConverter(accessTokenConverter);//outro que definimos
+		.accessTokenConverter(accessTokenConverter);//outro que definimos 
 	}
 
 	
